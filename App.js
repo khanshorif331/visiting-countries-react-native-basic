04529985214ec501e98c3d7074de58212a55308e
khanshorif331 <khanshorif331@gmail.com>
2022-05-30 20:32:46 +0600

    data loaded from the api

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import Countries from './components/Countries'
+import React, { useState, useEffect } from 'react'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text>Visiting Countriies!</Text>
+			<Countries></Countries>
+			<StatusBar style='auto' />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+})
---
+++ components/Countries.js
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+
+export default function Countries() {
+	const [countries, setCountries] = useState([])
+	useEffect(() => {
+		fetch('https://restcountries.com/v3.1/all')
+			.then(res => res.json())
+			.then(data => setCountries(data))
+	}, [])
+	return (
+		<View>
+			<Text>Countries : {countries.length}</Text>
+		</View>
+	)
+}
