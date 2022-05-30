9e703eadaa35192c90e21b0b826d60967dd87c1e
khanshorif331 <khanshorif331@gmail.com>
2022-05-30 21:16:25 +0600

    search functionality added

--- components/Countries.js
+++ components/Countries.js
@@ -1,22 +1,50 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, TextInput, StyleSheet } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import Country from './Country'
 
 export default function Countries() {
 	const [countries, setCountries] = useState([])
+	const [searched, setSearched] = useState([])
 	useEffect(() => {
 		fetch('https://restcountries.com/v3.1/all')
 			.then(res => res.json())
-			.then(data => setCountries(data))
+			.then(data => {
+				setCountries(data)
+				setSearched(data)
+			})
 	}, [])
+	const handleSearch = text => {
+		const filtered = countries.filter(country =>
+			country.name.common.includes(text)
+		)
+		setSearched(filtered)
+	}
 	return (
 		<View>
-			<Text>Countries : {countries.length}</Text>
+			<Text style={styles.header}>Countries : {searched.length}</Text>
+			<TextInput
+				onChangeText={handleSearch}
+				style={styles.input}
+			></TextInput>
 			<ScrollView>
-				{countries.map(country => (
+				{searched.map(country => (
 					<Country country={country}></Country>
 				))}
 			</ScrollView>
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	header: {
+		marginTop: 50,
+		fontSize: 40,
+		color: 'red',
+	},
+	input: {
+		height: 40,
+		margin: 12,
+		borderWidth: 1,
+		padding: 10,
+	},
+})
--- components/Country.js
+++ components/Country.js
@@ -1,10 +1,16 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 
 export default function Country({ country }) {
 	return (
 		<View>
-			<Text>Country : {country.name.common}</Text>
+			<Text style={{ fontSize: 20 }}>Country : {country.name.common}</Text>
+			<Image
+				source={{
+					uri: country.flags.png,
+				}}
+				style={{ width: 200, height: 200 }}
+			></Image>
 		</View>
 	)
 }
